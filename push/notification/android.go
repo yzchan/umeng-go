e7529415ce2d33fd0c44bffa3c4c8b96b1f94a86
chenyuzou <chenyuzou@qq.com>
2021-07-06 14:45:51 +0800

    .

--- push/notification/android.go
+++ push/notification/android.go
@@ -61,5 +61,6 @@ func (n *AndroidNotification) SetImage(img string) Notificationer {
 
 func (n *AndroidNotification) SetSilent() Notificationer {
 	n.Payload.SetDisplayType("message")
+	n.Payload.Body.SetCustom("")
 	return n
 }
